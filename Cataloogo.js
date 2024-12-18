ff4e668d16140d7d1525aabbccb047e58a956dc7
JosuCastr28 <josue.castro@euroliceo.edu.mx>
2024-12-18 15:52:15 -0600

    Update Cataloogo.js

--- Cataloogo.js
+++ Cataloogo.js
@@ -1,3 +1,14 @@
+// Cargar los productos desde el archivo JSON
+let products = [];
+fetch('https://raw.githubusercontent.com/JosuCastr28/catalogo-productos/main/Catalógo.json')
+   .then(response => response.json())
+   .then(data => {
+      products = data;
+      displayProducts(products); // Mostrar todos los productos al cargar
+   })
+   .catch(error => console.error('Error al cargar los productos:', error));
+
+// Función para mostrar los productos en el HTML
 function displayProducts(products) {
    const productList = document.getElementById('product-list');
    productList.innerHTML = ''; // Limpiar el contenedor antes de mostrar los productos
@@ -6,13 +17,13 @@ function displayProducts(products) {
       const productDiv = document.createElement('div');
       productDiv.classList.add('product');
       productDiv.innerHTML = `
-         <h3>${product.descripcion}</h3>
-         <p>Referencia: ${product.referencia}</p>
-         <p>Unidad de Medida: ${product.unidad_de_medida}</p>
-         <p>Familia: ${product.familia}</p>
-         <p>Almacén: ${product.almacen}</p>
-         <img src="imagenes/${product.imagen}" alt="${product.descripcion}" class="product-image">
+         <h3>${product.producto}</h3>
+         <p>Código: ${product.codigo}</p>
+         <p>Categoría: ${product.categoria}</p>
+         <p>Unidad: ${product.unidad}</p>
+         <img src="${product.imagen}" alt="${product.producto}" class="product-image">
       `;
       productList.appendChild(productDiv);
    });
 }
+
