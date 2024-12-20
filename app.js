c1593e201e231d3ec7bb32ec800b7bcdff18d68e
JosuCastr28 <josue.castro@euroliceo.edu.mx>
2024-12-20 12:21:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Ruta al archivo JSON
-const jsonFilePath = 'Catalogo.json';
+const jsonFilePath = 'Catalógo.json';
 
 // Elementos del DOM
 const productGrid = document.getElementById('product-grid');
